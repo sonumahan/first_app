221f320c32105b61dd95bee9ddc6ea8d4936b277
Sonu Mahan <sonu@omprakash.org>
2015-02-02 08:53:14 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubySapleApp::Application.config.secret_token = '73f8d0553dff1a1115a4f45efe69fe05f6717a8b4f1dbed326823ce3a773724cc307985a0216d0e63d62e40b6daa558cbdaf881eee25cc2aa11a3609382007c2'
